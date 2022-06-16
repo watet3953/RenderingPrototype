14b70ba91963382100e36b5bfe8a2629549bb824
Travis Waterman <106386185+watet3953@users.noreply.github.com>
2022-06-16 09:19:19 -0400

    Create Vector2.java

---
+++ Vector2.java
@@ -0,0 +1,14 @@
+// Travis Waterman
+// April 12th, 2022
+// Vector2.java
+// A class for storing vector2 data.
+
+class Vector2 {
+  double x = 0.0;
+  double y = 0.0;
+
+  public Vector2(double x1, double y1) {
+    x = x1;
+    y = y1;
+  }
+}
