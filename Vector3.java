fbd662cf194e9e781ba62dff2fdefd60f638c028
Travis Waterman <106386185+watet3953@users.noreply.github.com>
2022-06-16 09:19:44 -0400

    Create Vector3.java

---
+++ Vector3.java
@@ -0,0 +1,16 @@
+// Travis Waterman
+// April 12th, 2022 - May 31st, 2022
+// Vector3.java
+// A class for storing vector3 data.
+
+class Vector3 {
+  double x;
+  double y;
+  double z;
+
+  public Vector3(double x1, double y1, double z1) {
+    x = x1;
+    y = y1;
+    z = z1;
+  }
+}
